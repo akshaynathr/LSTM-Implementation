e79b2aee03ec5853b18aad92bbe52f4ab78bac33
Akshaynathr <akshaynathr@gmail.com>
2018-07-14 09:13:54 +0530

    define the maths functions

--- lstm.py
+++ lstm.py
@@ -1,1 +1,16 @@
-from tensorflow import tf
+import random
+
+import numpy as np 
+import math
+
+def sigmoid(x):
+    return 1./(1+np.exp(-x))
+
+def sigmoid_derivative(values):
+    """
+    derivative of sigmoid(x) = x *(1-x)
+    """
+    return values * (1- values)  
+
+def tanh_derivative ( values):
+    return 1.- values**2
