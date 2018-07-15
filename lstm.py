8cd773e654460a2cfd840db018e796e7301163e3
Akshaynathr <akshaynathr@gmail.com>
2018-07-15 17:11:33 +0530

    started the class LSTMParam to store parameters of the LSTM Network

--- lstm.py
+++ lstm.py
@@ -29,3 +29,31 @@ def create_rand_array(a,b,*args):
 
 """
 
+# LSTMParam Class Implementation
+""" 
+mm_cell_count : LSTM cell count
+x_dim = T_x  , the length of the input string
+
+"""
+
+class LSTMParam:
+    def __init__(self,mm_cell_count,x_dim):
+        self.mm_cell_count = mm_cell
+        self.x_dim   = x_dim
+        """ 
+        The weights used in LSTM are made from 
+        concatentaing the input text and activation values
+        W_ax =[a<i> : x<i>]
+
+        concat_length = size of cells + size of input (x)
+        """
+        concat_length  = x_dim + mm_cell
+
+        """ 
+        Create matrises for storing different weights
+
+        """
+        # output gate matrix
+        self.wo = create_rand_array(-0.01,0.01,mm_cell_count,concat_length)
+        self.wf = create_rand_array(-0.01,0.01,mm_cell_count,concat_length)
+        self.wi = create_rand_array(-0.01,0.01,mm_cell_count,concat_length)
